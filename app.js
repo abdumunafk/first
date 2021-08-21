6f316b84d93a5525d2df9162040c53ad1376f136
abdumunafk <83628290+abdumunafk@users.noreply.github.com>
2021-08-21 15:06:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+{ totalMemory as _totalMemory, freemem } from 'os';
+
+var x = 2e+3;
+
+//console.log('Total Memory:'+ totalMemory);
+
+console.log('Total Memory:${totalMemory}');
+
+console.log('Free Memory:${freeMemory}');
